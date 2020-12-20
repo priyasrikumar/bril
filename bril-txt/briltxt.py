5bd411314b542ae4a5633e80072161f3e8092e55
Goktug <gs2417@nyu.edu>
2020-12-20 17:25:38 -0500

    adding support for hof syntax

--- bril-txt/briltxt.py
+++ bril-txt/briltxt.py
@@ -22,8 +22,9 @@ start: func*
 func: FUNC ["(" arg_list? ")"] [tyann] "{" instr* "}"
 arg_list: | arg ("," arg)*
 arg: IDENT ":" type
-?instr: const | vop | eop | label
+?instr: const | vop | eop | anon | label
 
+anon.5: IDENT [tyann] "=" "anon" "(" [fnargs] ")" "{" instr* "}" ";"
 const.4: IDENT [tyann] "=" "const" lit ";"
 vop.3: IDENT [tyann] "=" op ";"
 eop.2: op ";"
@@ -31,14 +32,16 @@ label.1: LABEL ":"
 
 op: IDENT (FUNC | LABEL | IDENT)*
 
+fnargs: | IDENT ("," IDENT)*
 ?tyann: ":" type
 
 lit: SIGNED_INT  -> int
   | BOOL         -> bool
   | DECIMAL      -> float
 
-type: IDENT "<" type ">" -> paramtype
-    | IDENT              -> primtype
+type: IDENT "<" type ">"                                      -> paramtype
+    | IDENT                                                   -> primtype
+    | IDENT "<" (type ("," type)*)? ">" "," "<" (type?) ">"   -> fntype
 
 BOOL: "true" | "false"
 IDENT: ("_"|"%"|LETTER) ("_"|"%"|"."|LETTER|DIGIT)*
@@ -84,6 +87,9 @@ class JSONTransformer(lark.Transformer):
     def arg_list(self, items):
         return items
 
+    def fnargs(self, items):
+        return items
+
     def const(self, items):
         dest, type, val = items
         out = {
@@ -93,6 +99,20 @@ class JSONTransformer(lark.Transformer):
         }
         if type:
             out['type'] = type
+        return out
+
+    def anon(self, items):
+        dest, typ, args = items[0], items[1], items[2]
+        body = items[3:]
+        out = {
+            'op': 'anon',
+            'dest': str(dest),
+            'type': {'fun': typ},
+        }
+        if args:
+            out['args'] = args
+        if body:
+            out['instrs'] = body
         return out
 
     def vop(self, items):
@@ -117,6 +137,10 @@ class JSONTransformer(lark.Transformer):
             else:
                 args.append(str(item))
 
+        if opcode == 'apply':
+            funcs.append(args[0])
+            args = args[1:]
+
         out = {'op': opcode}
         if args:
             out['args'] = args
@@ -124,6 +148,9 @@ class JSONTransformer(lark.Transformer):
             out['funcs'] = funcs
         if labels:
             out['labels'] = labels
+        #if items['instrs']:
+         #   out['instrs'] = items['instrs']
+
         return out
 
     def eop(self, items):
@@ -151,6 +178,14 @@ class JSONTransformer(lark.Transformer):
     def primtype(self, items):
         return str(items[0])
 
+    def fntype(self, items):
+        out = {}
+        if items[1:-1]:
+            out['params'] = items[1:-1]
+        if items[-1]:
+            out['ret'] = items[-1]
+        return out
+
     def float(self, items):
         return float(items[0])
 
@@ -165,15 +200,26 @@ def parse_bril(txt):
 # Text format pretty-printer.
 
 def type_to_str(type):
-    if isinstance(type, dict):
+    if isinstance(type,dict) and type.get('fun'):
+        type = type.get('fun')
+        assert len(type) == 2
+        params = type.get('params')
+        ret = type.get('ret')
+        params = '{}'.format(', '.join(
+              '{}'.format(type_to_str(param))
+              for param in params
+          ))
+        return 'fun <{}>, <{}>' \
+          .format(params, type_to_str(ret))
+    elif isinstance(type, dict):
         assert len(type) == 1
         key, value = next(iter(type.items()))
-        return '{}<{}>'.format(key, type_to_str(value))
+        return '{}<{}>'.format(key, type_to_str(value))  
     else:
         return type
 
 
-def instr_to_string(instr):
+def instr_to_string(instr, idt):
     if instr['op'] == 'const':
         tyann = ': {}'.format(type_to_str(instr['type'])) \
             if 'type' in instr else ''
@@ -182,9 +228,28 @@ def instr_to_string(instr):
             tyann,
             str(instr['value']).lower(),
         )
+    elif instr['op'] == "anon":
+        args = ''
+        if instr.get('args'):
+            args = anon_args_to_string(instr.get('args'))
+        tyann = ': {}'.format(type_to_str(instr['type']))
+        print('{}{} = anon {} {{'.format(
+              instr['dest'],
+              tyann,
+              args))
+        for instr_or_label in instr['instrs']:
+          if 'label' in instr_or_label:
+              print_label(instr_or_label, idt)
+          else:
+              print_instr(instr_or_label, idt + 2)
+        return '}'
     else:
         rhs = instr['op']
-        if instr.get('funcs'):
+        if instr.get('funcs') and instr['op'] == 'apply':
+            rhs += ' {}'.format(' '.join(
+                '{}'.format(f) for f in instr['funcs']
+            ))
+        elif instr.get('funcs') and instr['op'] != 'apply':
             rhs += ' {}'.format(' '.join(
                 '@{}'.format(f) for f in instr['funcs']
             ))
@@ -206,12 +271,14 @@ def instr_to_string(instr):
             return rhs
 
 
-def print_instr(instr):
-    print('  {};'.format(instr_to_string(instr)))
+def print_instr(instr, idt):
+    idts = ' ' * idt
+    print('{}{};'.format(idts, instr_to_string(instr, idt)))
 
 
-def print_label(label):
-    print('.{}:'.format(label['label']))
+def print_label(label, idt):
+    idts = ' ' * idt
+    print('{}.{}:'.format(idts, label['label']))
 
 
 def args_to_string(args):
@@ -223,6 +290,15 @@ def args_to_string(args):
     else:
         return ''
 
+def anon_args_to_string(args):
+  if args:
+      return '({})'.format(', '.join (
+            '{}'.format(arg)
+            for arg in args
+        ))
+  else:
+    return ''
+
 
 def print_func(func):
     typ = func.get('type', 'void')
@@ -233,9 +309,9 @@ def print_func(func):
     ))
     for instr_or_label in func['instrs']:
         if 'label' in instr_or_label:
-            print_label(instr_or_label)
+            print_label(instr_or_label, 0)
         else:
-            print_instr(instr_or_label)
+            print_instr(instr_or_label, 2)
     print('}')
 
 
